7d9a6d71c1829cd417abf3005aa7b9597c1a1c9e
Tarek Ashraf <tarekshalaby2015@gmail.com>
2025-02-14 15:24:29 +0200

    llm factory done

--- src/main.py
+++ src/main.py
@@ -7,6 +7,8 @@ from contextlib import asynccontextmanager
 from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo.server_api import ServerApi
 from helpers.config import get_settings
+from stores.llm import LLMProviderFactory
+from stores.llm import LLMProviderEnum
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -15,6 +17,18 @@ async def lifespan(app: FastAPI):
     app.mongo_client = AsyncIOMotorClient(settings.MONGO_DB_URL)
     
     app.mongo_db = app.mongo_client.get_database(settings.MONGO_DB_DATABASE)
+    
+    llm_provider_factory = LLMProviderFactory(config= settings.model_dump())
+    
+    #setting generation client
+    app.generation_client = llm_provider_factory.intialize_provider(provider_name= settings.GENERATION_BACKEND)
+    app.generation_client.set_generagtion_model(model_id= settings.GENERATION_MODEL_ID)
+    
+    #setting embedding client 
+    app.embedding_client = llm_provider_factory.intialize_provider(provider_name= settings.EMBEDDING_BACKEND )
+    app.embedding_client.set_embedding_model(model_id= settings.EMBEDDING_MODEL_ID,
+                                             vector_size= settings.EMBEDDING_MODEL_SIZE)
+    
     yield
     app.mongo_client.close()
 
